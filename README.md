374d52046be1890624f19f3d70ebfab1ad8be820
shawnkoon <shawn9405@gmail.com>
2017-05-13 10:48:11 -0700

    Documents + s3_input object + lambda funciton tester code added

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # aws-quadtree-lambda
 AWS Lambda function that creates container jobs from s3 event.
+
+--------------------
+
+## Handlers:
+
+-If uploaded directly:
+
+`quadtree_lambda.launch_container`
+
+-If copy and pasted:
+
+`lambda_function.launch_container`
+
+
+--------------------
+
+# Reading.
+
+http://docs.aws.amazon.com/lambda/latest/dg/lambda-introduction-function.html
+
+http://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+https://aws.amazon.com/blogs/compute/better-together-amazon-ecs-and-aws-lambda/
--- quadtree_lambda.py
+++ quadtree_lambda.py
@@ -1,19 +1,31 @@
 '''
   For detailed information on API call, please checkout official boto3 website.
   => https://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Bucket.copy
-  
+
   Please checkout my github for more information.
   => https://github.com/shawnkoon/aws_python_practice
 
-  @purpose: Duplicate triggered object into specified output bucket.
+  @purpose: Receives an csv file from s3 bucket,
+            parse & creates a json tree node object and uploads it into dynamodb.
   python v3.6
 '''
 import json
 import boto3
 
 # Get s3 client.
-s3_client = boto3.client('s3')
+S3_CLIENT = boto3.client('s3')
 
 def launch_container(event, context):
+  '''
+  Lambda entry function that receives event & context.
+  '''
+
   # Print received object.
-  print("Received event: " + json.dumps(event, indent=2))
+  print('Received event: ' + json.dumps(event, indent=2))
+  print('context : ' + context.function_name)
+
+  src_bucket = event['Records'][0]['s3']['bucket']['name']
+  src_key = event['Records'][0]['s3']['object']['key']
+
+  print('src_bucket : ' + src_bucket)
+  print('src_key : ' + src_key)
