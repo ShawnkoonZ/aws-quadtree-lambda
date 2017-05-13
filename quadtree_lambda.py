49641ea2862303e5662a26ea6ac28e7c1c05f2de
shawnkoon <shawn9405@gmail.com>
2017-05-13 11:33:19 -0700

    Lambda testing json added + lambda function refactored to moduler structure

--- quadtree_lambda.py
+++ quadtree_lambda.py
@@ -14,18 +14,61 @@ import boto3
 
 # Get s3 client.
 S3_CLIENT = boto3.client('s3')
+DYNAMO_CLIENT = boto3.client('dynamodb')
+
+def get_bucket_name(event):
+    '''
+    Get bucket name from incomming event.
+    @param event : Received event.
+    '''
+    return event['Records'][0]['s3']['bucket']['name']
+
+def get_key_name(event):
+    '''
+    Get key name from incomming event.
+    @param event : Received event.
+    '''
+    return event['Records'][0]['s3']['object']['key']
+
+def to_string(byte_object):
+    '''
+    Converts Byte object into String object.
+    @param byte_object : Source Byte object.
+    -> Returns String object.
+    '''
+    return byte_object.decode("utf-8")
+
+def read_object(bucket_name, key_name):
+    '''
+    Reads object from given bucket with matching key.
+    @param bucket_name : Source bucket name.
+    @param key_name : Source key name.
+    -> Returns s3 object.
+    '''
+    return S3_CLIENT.get_object(
+        Bucket=bucket_name,
+        Key=key_name
+    )
+
+def print_detail(event, context):
+    '''
+    Prints the detail of the event & context.
+    @param event : received event.
+    @param context : current context info.
+    '''
+    print('Received event: ' + json.dumps(event, indent=2))
+    print('Current function: ' + context.function_name)
 
 def launch_container(event, context):
-  '''
-  Lambda entry function that receives event & context.
-  '''
+    '''
+    Lambda entry function that receives event & context.
+    @param event : received event.
+    @param context : current context info.
+    '''
+    print_detail(event, context)
+    src_bucket = get_bucket_name(event)
+    src_key = get_key_name(event)
 
-  # Print received object.
-  print('Received event: ' + json.dumps(event, indent=2))
-  print('context : ' + context.function_name)
-
-  src_bucket = event['Records'][0]['s3']['bucket']['name']
-  src_key = event['Records'][0]['s3']['object']['key']
-
-  print('src_bucket : ' + src_bucket)
-  print('src_key : ' + src_key)
+    s3_object = read_object(src_bucket, src_key)
+    s3_object_list = to_string(s3_object['Body'].read()).split('\n')
+    print(s3_object_list)
